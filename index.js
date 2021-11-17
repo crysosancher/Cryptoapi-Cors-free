b6731424e47feaf669cd4d664b56611fc9e65f8f
crysosancher <vibhupandey.vp@gmail.com>
2021-11-17 21:56:54 +0530

    added express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path=require('path')
+const app = express()
+const port = 3000
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+app.get('/', (req, res) => {
+
+  res.json({"Himanshu Lomda": 69})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
